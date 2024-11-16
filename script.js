07299cdaf9356e602ce855d82f52781d8193705b
Ayushcoder07 <108988850+Ayushcoder07@users.noreply.github.com>
2024-11-16 18:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Wait for the DOM to be fully loaded before executing code
 document.addEventListener('DOMContentLoaded', () => {
-    let board = null; // Initialize the chessboard
-    const game = new Chess(); // Create new Chess.js game instance
-    const moveHistory = document.getElementById('move-history'); // Get move history container
-    let moveCount = 1; // Initialize the move count
-    let userColor = 'w'; // Initialize the user's color as white
+    let board = null; 
+    const game = new Chess(); 
+    const moveHistory = document.getElementById('move-history');
+    let moveCount = 1; 
+    let userColor = 'w'; 
 
-    // Function to make a random move for the computer
     const makeRandomMove = () => {
         const possibleMoves = game.moves();
 
@@ -17,25 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
             const move = possibleMoves[randomIdx];
             game.move(move);
             board.position(game.fen());
-            recordMove(move, moveCount); // Record and display the move with move count
-            moveCount++; // Increament the move count
+            recordMove(move, moveCount);
+            moveCount++;
         }
     };
 
-    // Function to record and display a move in the move history
     const recordMove = (move, count) => {
         const formattedMove = count % 2 === 1 ? `${Math.ceil(count / 2)}. ${move}` : `${move} -`;
         moveHistory.textContent += formattedMove + ' ';
-        moveHistory.scrollTop = moveHistory.scrollHeight; // Auto-scroll to the latest move
+        moveHistory.scrollTop = moveHistory.scrollHeight;
     };
 
-    // Function to handle the start of a drag position
     const onDragStart = (source, piece) => {
-        // Allow the user to drag only their own pieces based on color
         return !game.game_over() && piece.search(userColor) === 0;
     };
 
-    // Function to handle a piece drop on the board
     const onDrop = (source, target) => {
         const move = game.move({
             from: source,
@@ -46,16 +40,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if (move === null) return 'snapback';
 
         window.setTimeout(makeRandomMove, 250);
-        recordMove(move.san, moveCount); // Record and display the move with move count
+        recordMove(move.san, moveCount);
         moveCount++;
     };
 
-    // Function to handle the end of a piece snap animation
     const onSnapEnd = () => {
         board.position(game.fen());
     };
 
-    // Configuration options for the chessboard
     const boardConfig = {
         showNotation: true,
         draggable: true,
@@ -63,15 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
         onDragStart,
         onDrop,
         onSnapEnd,
-        moveSpeed: 'fast',
-        snapBackSpeed: 500,
-        snapSpeed: 100,
     };
 
-    // Initialize the chessboard
     board = Chessboard('board', boardConfig);
 
-    // Event listener for the "Play Again" button
     document.querySelector('.play-again').addEventListener('click', () => {
         game.reset();
         board.start();
@@ -80,27 +67,20 @@ document.addEventListener('DOMContentLoaded', () => {
         userColor = 'w';
     });
 
-    // Event listener for the "Set Position" button
     document.querySelector('.set-pos').addEventListener('click', () => {
         const fen = prompt("Enter the FEN notation for the desired position!");
-        if (fen !== null) {
-            if (game.load(fen)) {
-                board.position(fen);
-                moveHistory.textContent = '';
-                moveCount = 1;
-                userColor = 'w';
-            } else {
-                alert("Invalid FEN notation. Please try again.");
-            }
+        if (fen !== null && game.load(fen)) {
+            board.position(fen);
+            moveHistory.textContent = '';
+            moveCount = 1;
+            userColor = 'w';
+        } else {
+            alert("Invalid FEN notation. Please try again.");
         }
     });
 
-    // Event listener for the "Flip Board" button
     document.querySelector('.flip-board').addEventListener('click', () => {
         board.flip();
-        makeRandomMove();
-        // Toggle user's color after flipping the board
         userColor = userColor === 'w' ? 'b' : 'w';
     });
-
 });
